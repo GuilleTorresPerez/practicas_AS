bb2c7ac11ea8922f36a24ce7282c550885a9f751
Curryllo <149586390+Curryllo@users.noreply.github.com>
2024-02-23 10:45:51 +0100

    ejercicio2

--- practica_2/practica2_2.sh
+++ practica_2/practica2_2.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 #842545, Valero Casajus, Curro, [M], [3], [B]
 #843078, Torres Perez, Guillermo, [M], [3], [B] 
+
+for param in "$@"
+do
+    if [ -f "$param" ]
+    then
+       more "$param"
+    else
+       echo -n "$param no es un fichero"
+    fi
+done
