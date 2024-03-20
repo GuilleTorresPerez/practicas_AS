b33eed665fb3a8599d12b929187d5e674e1ce035
Guillermo Torres Pérez <60155590+GuilleTorresPerez@users.noreply.github.com>
2024-03-20 12:37:06 +0100

    Añadir conexión maquina virtual

--- practica_3/practica_3.sh
+++ practica_3/practica_3.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
+#842545, Valero Casajus, Curro, [M], [3], [B]
+#843078, Torres Perez, Guillermo, [M], [3], [B] 
+
+# a) 
+# b) 
+# c) 
+# d)
+# e)
+# f)
+# g)
+# h)
+# i)
+# j)
+# k)
+# l)
+# m)
+# n)
+# o)
+# p)
+# q)
 
 if [ $(id -u) -ne "0" ]
 then
    echo "Este script necesita permisos de administración"
-   exit
+   exit -1
 fi
 
 if [ $# != 2 ]
