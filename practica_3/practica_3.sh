c250485d22254e0f80ddbde80acfd80e0a99ce24
Curryllo <149586390+Curryllo@users.noreply.github.com>
2024-03-22 19:35:37 +0100

    fin

--- practica_3/practica_3.sh
+++ practica_3/practica_3.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+#842545, Valero Casajus, Curro, [M], [3], [B]
+#843078, Torres Perez, Guillermo, [M], [3], [B] 
 #FUNCIONES
 fecha_actual=$(date +"%Y-%m-%d")
 fecha_nueva=$(date -d "$fecha_actual + 30 days" +"%Y-%m-%d")
