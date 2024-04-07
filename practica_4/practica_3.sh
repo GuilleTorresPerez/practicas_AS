d3902dff53754e2fff7a5f67514163e67ccfc21e
Guillermo Torres Pérez <60155590+GuilleTorresPerez@users.noreply.github.com>
2024-04-07 21:07:36 +0200

    practica4 (conecta pero sun sin test)

---
+++ practica_4/practica_3.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+#842545, Valero Casajus, Curro, [M], [3], [B]
+#843078, Torres Perez, Guillermo, [M], [3], [B] 
+#FUNCIONES
+fecha_actual=$(date +"%Y-%m-%d")
+fecha_nueva=$(date -d "$fecha_actual + 30 days" +"%Y-%m-%d")
+
+addUser(){
+ local login_name="$1"
+ local contrasena="$2"
+ local nombreCompleto="$3"
+
+ #Comprobar si los campos están vacíos
+ if [ -z "$login_name" ] || [ -z "$contrasena" ] || [ -z "$nombreCompleto" ]
+ then
+    echo "Campo invalido"
+    exit 1
+ fi
+
+ #Comprobar si el usuario existe revisando el fichero /etc/passwd
+ if grep -q "^$login_name:" /etc/passwd; then
+    echo "El usuario $login_name ya existe"
+ fi
+
+ #Crear el usuario
+ useradd -K UID_MIN=1815 -U -m -e $fecha_nueva -k /etc/skel -c "$nombreCompleto" "$login_name"
+ #Asignar la contraseña
+ echo "$login_name:$contrasena" | chpasswd
+ #Desbloquear usuario
+ usermod -U "$login_name" &>/dev/null
+ echo "$nombreCompleto ha sido creado"
+}
+
+
+removeUser(){
+   #Comprobar si el usuario existe
+   grep -q "$1" /etc/passwd
+   if [ $? -eq 1 ]; then 
+      return 
+   fi
+
+   #Obtenemos el directorio home del user
+   home=$(grep "$1" /etc/passwd | cut -d ':' -f 6) 
+   tar czfp /extra/backup/"$1".tar "$home" &> /dev/null
+   if [ $? -eq 0 ]; then								# Por último, eliminamos el usuario
+		userdel -r "$1" &> /dev/null	
+	fi
+}
+
+crearCarpetaBackup(){
+   if [ ! -d /extra/backup ]; then					# Creamos el directorio de backup
+		mkdir -p /extra/backup
+	fi
+}
+
+
+
+
+#PROGRAMA PRINCIPAL
+
+if [ $(id -u) -ne "0" ]
+then
+   echo "Este script necesita privilegios de administracion"
+   exit 1
+fi
+
+if [ $# != 2 ]
+then
+    echo "Numero incorrecto de parametros"
+    exit 1
+fi
+
+OLDIFS=$IFS
+IFS=,
+if [ "$1" = "-a" ]
+then
+   while read -r usuario pass nombre basura
+   do
+     addUser "$usuario" "$pass" "$nombre"
+   done < "$2"
+elif [ "$1" = "-s" ]; then
+   crearCarpetaBackup
+   while read usuario passwd nombre basura
+   do 
+      removeUser "$usuario"
+   done < "$2"
+else
+   echo "Opcion invalida"
+   exit 1
+fi
+IFS=$OLDIFS
---
+++ practica_4/practica_4.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#842545, Valero Casajus, Curro, [M], [3], [B]
+#843078, Torres Perez, Guillermo, [M], [3], [B] 
+
+if [ "$#" -ne 3 ]
+then
+	echo "Numero incorrecto de parametros" 1>&2
+	exit 1
+fi
+
+OLDIFS=$IFS
+IFS=,
+while read -r ip basura
+do
+    if ! ping -c 1 "$ip" &> /dev/null; then
+        echo "$ip no es accesible" 1>&2
+        continue
+    fi
+
+    scp practica_3.sh as@${ip}:~/ &> /dev/null
+    scp $2 as@${ip}:~/ &> /dev/null
+    ssh -i $HOME/.ssh/id_as_ed25519 -n as@${ip} "sudo chmod u+x practica_3.sh"
+    ssh -i $HOME/.ssh/id_as_ed25519 -n as@${ip} "sudo ./practica_3.sh $1 $2"
+done < "$3"
+IFS=$OLDIFS
+
